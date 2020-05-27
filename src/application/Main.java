8ed67cdbd750c60fc29d8133df2b32efcfe4533f
Victor Pereira <victor.cordeiro2@gmail.com>
2020-05-27 19:56:20 -0300

    insert, update and delete implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 
-import java.util.List;
+import java.util.Date;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,12 +12,28 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		List<Seller> list = sellerDao.findAll();
-		for(Seller obj : list) {
-			System.out.println(obj);
-		}
+		System.out.println("== seller insert ==");
+		Department dep = new Department(2, null);
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());	
+		
+		System.out.println("== seller update ==");
+		Seller seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
+		
+		System.out.println("== seller delete ==");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed!");
+		sc.close();
+	
 	}
 
 }
